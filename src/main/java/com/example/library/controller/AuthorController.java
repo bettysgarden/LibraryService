6f7469d8406ccf8cd7e1241fc4dddd3019b7e217
bettysgarden <99659178+bettysgarden@users.noreply.github.com>
2023-06-11 18:35:09 +0300

    added AuthorController.java

--- src/main/java/com/example/library/controller/AuthorController.java
+++ src/main/java/com/example/library/controller/AuthorController.java
@@ -1,18 +1,62 @@
 package com.example.library.controller;
 
+import com.example.library.entity.Author;
 import com.example.library.service.Implement.AuthorService;
-import com.example.library.service.Implement.BooksService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/authors")
 public class AuthorController {
-    private static final Logger logger = LoggerFactory.getLogger(AuthorService.class);
-
+    private final AuthorService authorService;
 
     @Autowired
-    private AuthorService authorService;
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
 
+    @GetMapping
+    public ResponseEntity<List<Author>> getAllAuthors() {
+        try {
+            List<Author> authors = authorService.getAll();
+            return ResponseEntity.ok(authors);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> getAuthorById(@PathVariable Long id) {
+        try {
+            Optional<Author> author = authorService.findById(id);
+            return author.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Author> createAuthor(@RequestBody Author author) {
+        try {
+            authorService.save(author);
+            return ResponseEntity.status(HttpStatus.CREATED).body(author);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAuthor(@PathVariable Long id) {
+        try {
+            authorService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
